a828b9d776369ba34dec6809cae589e030368692
Khanghcmut <138753176+Khanghcmut@users.noreply.github.com>
2024-01-10 10:17:09 +0700

    change to utc

--- jsontocsv.py
+++ jsontocsv.py
@@ -5,14 +5,17 @@ def seconds(time):
         t0=datetime(1970,1,1)
         delta=time-t0
         return delta.total_seconds()
+def to_date_time(x):
+    return datetime.fromtimestamp(x)
 def json_to_csv(i):
-    jsonfile='./Desktop/nckh_sv/dataset/EMA/response/Stress'+f'/Stress_u{i:02d}.json'
-    csvfile=f'/Users/khangphan/Desktop/Stress_csv_new/Stress_u{i:02d}.csv'
+    jsonfile='./dataset/EMA/response/Stress'+f'/Stress_u{i:02d}.json'
+    csvfile=f'./Stress_csv_new1/Stress_u{i:02d}.csv'
     print(csvfile)
     df=pd.read_json(jsonfile)
     df=df[['level','resp_time']]
     df['resp_time']=df['resp_time']
-    df['time']=df['resp_time'].apply(lambda time: seconds(time))
+    df['time']=df['resp_time'].apply(lambda time: seconds(time))-3600*5
+    df['resp_time']=df['time'].apply(lambda x: to_date_time(x))
     if i==0:
         print(df)
 
@@ -23,6 +26,8 @@ for i in range(60):
     try:
         json_to_csv(i)
     except:
+        print('error')
+
         pass
 
 print(1)
